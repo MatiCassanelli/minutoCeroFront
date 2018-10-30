d09a8bc5bd9f714a1d9fd9f7d01ce5585b0a1dc8
Felipe Toledo <felipetoledo4815@gmail.com>
2018-10-30 19:06:09 -0300

    Se agregó servicio para actualizar visto=true de las notificaciones

--- src/services/notificacionService.ts
+++ src/services/notificacionService.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 import * as global from '../app/app.global';
 import {Jugador} from '../app/models/jugador';
 import {Predio} from '../app/models/predio';
@@ -23,5 +23,9 @@ export class NotificacionService {
 
   responder(id, tipoNotificacion, respuesta) {
     return this.http.put<any>(this.api + '/solicitudes/' + tipoNotificacion + '/' + id, respuesta, global.httpOptions);
+  }
+
+  updateNotificacion(id) {
+    return this.http.put<any>(this.api + '/notificaciones/' + id, null, global.httpOptions);
   }
 }
