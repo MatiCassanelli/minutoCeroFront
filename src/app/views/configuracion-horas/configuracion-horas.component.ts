a45f8a90aa5fcfb001be2365c87730c91adbfb2c
MatiCassanelli <matiascassanelli@gmail.com>
2018-11-24 14:41:08 -0300

    Posibilidad de elegir si tiene o luz artificial y definir sus horarios

--- src/app/views/configuracion-horas/configuracion-horas.component.ts
+++ src/app/views/configuracion-horas/configuracion-horas.component.ts
@@ -36,14 +36,13 @@ export class ConfiguracionHorasComponent implements OnInit {
     this.defaultDate.setMinutes(this.redondearHora(today.getHours(), today.getMinutes())[1]);
     this.predioService.getPredio(localStorage.getItem('id')).subscribe(res => {
       if(res.configHorario){
-        this.horarios.dia.desde = moment.utc(res.configHorario.dia.desde).format('HH:mm');
-        this.horarios.dia.hasta = moment.utc(res.configHorario.dia.hasta).format('HH:mm');
-        debugger;
-        if(this.horarios.noche){
-          this.horarios.noche.desde = moment.utc(res.configHorario.noche.desde).format('HH:mm');
-          this.horarios.noche.hasta = moment.utc(res.configHorario.noche.hasta).format('HH:mm');
+        this.horarios.dia.desde = moment.utc(res.configHorario.dia.desde).add('h', 3).toDate();
+        this.horarios.dia.hasta = moment.utc(res.configHorario.dia.hasta).add('h', 3).toDate();
+        if(this.horarios.noche.desde){
+          this.horarios.noche.desde = moment.utc(res.configHorario.noche.desde).toDate();
+          this.horarios.noche.hasta = moment.utc(res.configHorario.noche.hasta).toDate();
         }
-        this.sendHorarios();
+        // this.sendHorarios();
       }
     });
   }
@@ -55,29 +54,39 @@ export class ConfiguracionHorasComponent implements OnInit {
   }
 
   sendHorarios() {
+    let horariosFinal = {
+      dia: {
+        desde: null,
+        hasta: null,
+      }
+    };
     if (this.horarios.dia.desde instanceof Date)
-      this.horarios.dia.desde = moment.utc().hours(this.horarios.dia.desde.getHours()).minutes(this.horarios.dia.desde.getMinutes()).format();
+      horariosFinal.dia.desde = moment.utc().hours(this.horarios.dia.desde.getHours()).minutes(this.horarios.dia.desde.getMinutes());
+// console.log(this.horarios.dia.desde)
     else
       this.horarios.dia.desde = moment.utc().hours(this.horarios.dia.desde.toString().split(':')[0]).minutes(this.horarios.dia.desde.toString().split(':')[1]).toDate();
     if (this.horarios.dia.hasta instanceof Date)
-      this.horarios.dia.hasta = moment.utc().hours(this.horarios.dia.hasta.getHours()).minutes(this.horarios.dia.hasta.getMinutes()).format();
+      horariosFinal.dia.hasta = moment.utc().hours(this.horarios.dia.hasta.getHours()).minutes(this.horarios.dia.hasta.getMinutes());
+      // console.log(this.horarios.dia.hasta)
     else
       this.horarios.dia.hasta = moment.utc().hours(this.horarios.dia.hasta.toString().split(':')[0]).minutes(this.horarios.dia.hasta.toString().split(':')[1]).toDate();
-    debugger;
     if(this.luces) {
+      horariosFinal['noche'] = {
+        desde: null,
+        hasta: null
+      }
       if (this.horarios.noche.desde instanceof Date)
-        this.horarios.noche.desde = moment.utc().hours(this.horarios.noche.desde.getHours()).minutes(this.horarios.noche.desde.getMinutes()).format();
+        horariosFinal['noche'].desde = moment.utc().hours(this.horarios.noche.desde.getHours()).minutes(this.horarios.noche.desde.getMinutes());
       else
         this.horarios.noche.desde = moment.utc().hours(this.horarios.noche.desde.toString().split(':')[0]).minutes(this.horarios.noche.desde.toString().split(':')[1]).toDate();
       if (this.horarios.noche.hasta instanceof Date)
-        this.horarios.noche.hasta = moment.utc().hours(this.horarios.noche.hasta.getHours()).minutes(this.horarios.noche.hasta.getMinutes()).format();
+        horariosFinal['noche'].hasta = moment.utc().hours(this.horarios.noche.hasta.getHours()).minutes(this.horarios.noche.hasta.getMinutes());
       else
         this.horarios.noche.hasta = moment.utc().hours(this.horarios.noche.hasta.toString().split(':')[0]).minutes(this.horarios.noche.hasta.toString().split(':')[1]).toDate();
-
     }
 
     this.stepEmit.emit({
-      configHoras: this.horarios
+      configHoras: horariosFinal
     });
     // this.predioService.setConfiguracionHorarios(this.horarios).subscribe(res => {
     //   console.log(res);
